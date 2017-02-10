03db3ddcb86e767c1f20ffd9d7794ca5185a1dcc
JeffDev <jeffdev74@gmail.com>
2017-02-09 23:38:08 -0800

    working on the ui system

--- Assets/Code/InventorySystem/UI/UIInventory.cs
+++ Assets/Code/InventorySystem/UI/UIInventory.cs
@@ -1,9 +1,70 @@
-﻿using UnityEngine;
+﻿using System;
+using FPS.UI;
+using UnityEngine;
 
 namespace FPS.InventorySystem.UI
 {
-	public class UIInventory : MonoBehaviour
-	{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class UIInventory : MonoBehaviour, IUIPanel
+    {
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+        }
 
-	}
+        private bool _isActive;
+        public bool IsActive
+        {
+            get { return _isActive; }
+        }
+
+        private string _panelName;
+        public string PanelName
+        {
+            get { return _panelName; }
+        }
+
+        private string _panelUniqueUUID;
+        public string PanelUniqueUUID
+        {
+            get { return _panelUniqueUUID; }
+            set { _panelUniqueUUID = value; }
+        }
+
+        private Transform _theTransform;
+        public Transform TheTransform
+        {
+            get
+            {
+                if(_theTransform == null)
+                {
+                    _theTransform = transform;
+                }
+                return _theTransform;
+            }
+        }
+
+        private CanvasGroup _theCanvasGroup;
+        private CanvasGroup TheCanvasGroup
+        {
+            get
+            {
+                if(_theCanvasGroup == null)
+                {
+                    _theCanvasGroup = GetComponent<CanvasGroup>();
+                }
+                return _theCanvasGroup;
+            }
+        }
+
+        public void TogglePanel(bool state)
+        {
+            TheCanvasGroup.alpha =  state ? 1 : 0;
+            TheCanvasGroup.interactable = state;
+            TheCanvasGroup.blocksRaycasts = state;
+            // Are we going to animate the panels??
+            // AnimatePanel(state)
+        }
+    }
 }
